0c2467d150f6232295a21f47a13188aabd05ea6e
Ramsey Jordan <ramsey.jordan.84@gmail.com>
2019-04-03 16:37:02 -0400

    Created partials and notFound page

--- app.js
+++ app.js
@@ -5,25 +5,24 @@ const app = express();
 // View engine must be specified to look for ejs
 app.set('view engine', 'ejs');
 
+// Use this to access the function in routes/index.js
+const routes = require('./routes');
+
+// Specifies the directory of static assets
+const path = require('path');
+// Tells express the static assets are in the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Routes
 
 // Home Route
-app.get('/', function(req, res) {
-  res.render('home', {
-    title : 'Star Wars Movies',
-  });
-});
+app.get('/', routes.home);
 
 // Single
-app.get('/star_wars_episode/:episode_number?', function(req, res) {
-  let episode_number = req.params.episode_number;
-  res.send('this is the page for episode ' + episode_number)
-})
+app.get('/star_wars_episode/:episode_number?', routes.movie_single);
 
 // notFound, This needs to go last
-app.get('*', function(req, res) {
-  res.send('This is not the page you are looking for!');
-});
+app.get('*', routes.notFound);
 
 app.listen(3000, function() {
   console.log('I’m one with the Force. The Force is with me.');
---
+++ routes/index.js
@@ -0,0 +1,57 @@
+// JSON data must be required to have access to it
+const moviesJSON = require('../movies.json');
+
+// Home Route
+exports.home = function(req, res) {
+
+  // Grabs the movies array and stores it in a movies variable
+  const movies = moviesJSON.movies
+
+  res.render('home', {
+    title : 'Star Wars Movies',
+    movies : movies
+  });
+};
+
+// Movie_Single
+exports.movie_single = function(req, res) {
+  // This is the number of the episode that is taken from the url. The number is then stored in the episode number variable
+  let episode_number = req.params.episode_number;
+
+  const movies = moviesJSON.movies;
+
+  if(episode_number >= 1 && episode_number <= 6) {
+
+    // Captures the movie # in the array - 1 because the array is 0 based
+    let movie = movies[episode_number - 1];
+
+    // This creates a variable called title that pulls the JSON movie.title data
+    const title = movie.title;
+
+    const main_characters = movie.main_characters;
+
+    res.render('movie_single', {
+      movies : movies,
+      title : title,
+      movie : movie,
+      main_characters : main_characters
+    });
+  } else {
+    res.render('notFound', {
+      movies : movies,
+      title : 'This is not the page you are looking for.'
+    })
+  }
+
+
+};
+
+// notFound, This needs to go last
+exports.notFound = function(req, res) {
+  const movies = moviesJSON.movies
+
+  res.render('notFound', {
+    movies : movies,
+    title : 'This is not the page you are looking for.'
+  })
+};
